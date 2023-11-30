9ef8a826fe0c24e231eb9b8db2b6af22796894f7
Dennis Woithe <woithe@trinext.de>
2023-11-30 13:26:36 +0100

    feat(Env & Random)

---
+++ src/main/java/de/trinext/util/internal/annotations/EvilShitFuckery.java
@@ -0,0 +1,30 @@
+package de.trinext.util.internal.annotations;
+
+import java.lang.annotation.*;
+
+/**
+ * Functions that use this annotation are highly unstable
+ * and plainly ignore what you think you know about Java.
+ * <p>
+ * Comment these functions with a detailed explanation of what they do.
+ *
+ * @author Dennis Woithe
+ */
+@Retention(RetentionPolicy.SOURCE)
+@Target(ElementType.METHOD)
+@SuppressWarnings("unused")
+public @interface EvilShitFuckery {
+
+    /** Description of the unexpected behaviour. */
+    BlackMagic[] blackMagic();
+
+    enum BlackMagic {
+        MODIFIES_PRIVATE_FIELD,
+        MODIFIES_FINAL_FIELD,
+        MODIFIES_IMMUTABLE_COLLECTION,
+        INVOKES_PRIVATE_METHOD,
+        USES_PRIVATE_CONSTANT,
+        MODIFIES_JDK_BEHAVIOUR,
+    }
+
+}
