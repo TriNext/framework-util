80896b72c358cfad1ff5daa4c0a9028d63f1a230
Dennis Woithe <146708358+TriNextDennis@users.noreply.github.com>
2023-12-07 10:53:54 +0100

    0.0.1 -> 0.0.3 (#11)
    * feat(Env & Random) (#1)
    
    * feat(Env & Random)
    
    * Updated .idea/inspectionProfiles/
    
    * Updated .idea/codeStyles/
    
    * Update Env.java
    
    ---------
    
    Co-authored-by: TriNextTony <ext.jenkins@trinext.de>
    
    * Package Refactor & HttpBuilder (#5)
    
    * feat(Env & Random)
    
    * Updated .idea/inspectionProfiles/
    
    * Updated .idea/codeStyles/
    
    * Update Env.java
    
    * feat(Package Refactor & HttpBuilder)
    
    * Delete src/main/java/de/trinext/util/Env.java
    
    ---------
    
    Co-authored-by: TriNextTony <ext.jenkins@trinext.de>
    
    * added "how to publish section" (#4)
    
    * Update README.md
    
    * Update README.md
    
    * added UrlQueryBuilderTest (#6)
    
    * test(url query builder 100% coverage)
    
    * feat(removed teams notification)
    
    * feat(HttpMethod)
    
    * feat(Env & Random)
    
    * Updated .idea/inspectionProfiles/
    
    * Updated .idea/codeStyles/
    
    * Update Env.java
    
    * feat(Package Refactor & HttpBuilder)
    
    * Delete src/main/java/de/trinext/util/Env.java
    
    * feat(HttpMethod)
    
    * fix(HttpMethod)
    
    * fix(Address Comments in #7)
    
    * fix(imports)
    
    ---------
    
    Co-authored-by: TriNextTony <ext.jenkins@trinext.de>
    Co-authored-by: MattisKunstmann <mattis.kunstmann@trinext.de>
    
    * MIME-Types (#9)
    
    * feat(Env & Random)
    
    * Updated .idea/inspectionProfiles/
    
    * Updated .idea/codeStyles/
    
    * Update Env.java
    
    * feat(Package Refactor & HttpBuilder)
    
    * Delete src/main/java/de/trinext/util/Env.java
    
    * feat(HttpMethod)
    
    * fix(HttpMethod)
    
    * fix(Address Comments in #7)
    
    * fix(imports)
    
    * feat(Mime Types)
    
    * fix(gitignore)
    
    ---------
    
    Co-authored-by: TriNextTony <ext.jenkins@trinext.de>
    Co-authored-by: MattisKunstmann <mattis.kunstmann@trinext.de>
    
    * fix(imports) (#10)
    
    ---------
    
    Co-authored-by: TriNextTony <ext.jenkins@trinext.de>
    Co-authored-by: Mattis Kunstmann <146000656+TriNextMattis@users.noreply.github.com>
    Co-authored-by: MattisKunstmann <mattis.kunstmann@trinext.de>

---
+++ src/main/java/de/trinext/framework/util/env/EnvHelper.java
@@ -0,0 +1,90 @@
+package de.trinext.framework.util.env;
+
+import java.util.*;
+
+import de.trinext.framework.util.internal.annotations.EvilShitFuckery;
+
+import static de.trinext.framework.util.internal.annotations.EvilShitFuckery.BlackMagic.*;
+
+/**
+ * @author Dennis Woithe
+ */
+@SuppressWarnings({"unused", "WeakerAccess", "DuplicateStringLiteralInspection"})
+public final class EnvHelper {
+
+    private EnvHelper() {
+        throw new AssertionError();
+    }
+
+    /**
+     * The environment variables passed to the JVM at startup.
+     */
+    @SuppressWarnings({"StaticCollection", "PublicStaticCollectionField"})
+    public static final Map<String, String> ORIGINAL_ENVIRONMENT = Map.copyOf(System.getenv());
+
+    private static final String PROCESS_ENVIRONMENT_CLASS = "java.lang.ProcessEnvironment";
+
+    /**
+     * Overwrites the environment variables passed to the JVM at startup.
+     * This should only be used in tests.
+     * Use {@code EnvHelper.setEnv(EnvHelper.ORIGINAL_ENVIRONMENT)} to reset after the test.
+     * Modifies the behaviour of:
+     * <ul>
+     *     <li>{@link System#getenv()}</li>
+     *     <li>{@link System#getenv(String)}}</li>
+     * </ul>
+     *
+     * @throws IllegalStateException When breaking eventually.
+     */
+    @EvilShitFuckery(blackMagic = {MODIFIES_FINAL_FIELD, MODIFIES_PRIVATE_FIELD, MODIFIES_JDK_BEHAVIOUR, MODIFIES_IMMUTABLE_COLLECTION})
+    @SuppressWarnings({"SpellCheckingInspection", "OverlyLongMethod", "NestedTryStatement", "OverlyNestedMethod"})
+    public static void setEnv(Map<String, String> newEnvVars) {
+        try {
+            try {
+                var processEnvironmentClass = Class.forName(PROCESS_ENVIRONMENT_CLASS);
+                var theEnvironmentField = processEnvironmentClass.getDeclaredField("theEnvironment");
+                theEnvironmentField.setAccessible(true);
+                var env = (Map<String, String>) theEnvironmentField.get(null);
+                env.clear();
+                env.putAll(newEnvVars);
+                var theCaseInsensitiveEnvironmentField = processEnvironmentClass.getDeclaredField("theCaseInsensitiveEnvironment");
+                theCaseInsensitiveEnvironmentField.setAccessible(true);
+                var cienv = (Map<String, String>) theCaseInsensitiveEnvironmentField.get(null);
+                cienv.clear();
+                cienv.putAll(newEnvVars);
+            } catch (NoSuchFieldException e) {
+                var classes = Collections.class.getDeclaredClasses();
+                var env = System.getenv();
+                for (var cls : classes) {
+                    if ("java.util.Collections$UnmodifiableMap".equals(cls.getName())) {
+                        var field = cls.getDeclaredField("m");
+                        field.setAccessible(true);
+                        var obj = field.get(env);
+                        var map = (Map<String, String>) obj;
+                        map.clear();
+                        map.putAll(newEnvVars);
+                    }
+                }
+            }
+        } catch (ClassNotFoundException | IllegalAccessException | NoSuchFieldException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    /**
+     * Adds an environment to this run of the JVM.
+     *
+     * @see #setEnv(Map)
+     */
+    @EvilShitFuckery(blackMagic = {MODIFIES_FINAL_FIELD, MODIFIES_PRIVATE_FIELD, MODIFIES_JDK_BEHAVIOUR, MODIFIES_IMMUTABLE_COLLECTION})
+    public static void addEnvVariable(String key, String value) {
+        if (key == null || key.isBlank())
+            throw new IllegalArgumentException("Env-Variable key must not be null or blank");
+        if (value == null || value.isBlank())
+            throw new IllegalArgumentException("Default value must not be null or blank");
+        var vars = new HashMap<>(System.getenv());
+        vars.put(key, value);
+        setEnv(vars);
+    }
+
+}
---
+++ src/test/java/framework/net/HttpBuilderTest.java
@@ -0,0 +1,9 @@
+package framework.net;
+
+/**
+ * @author Dennis Woithe
+ */
+final class HttpBuilderTest {
+
+
+}
