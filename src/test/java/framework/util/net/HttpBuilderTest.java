545984e127a8823b428303374d2b1a277d18e24f
TriNextSep <150134652+TriNextSep@users.noreply.github.com>
2023-12-12 09:09:00 +0100

    HTTPBuilder Tests.

--- src/test/java/framework/util/net/HttpBuilderTest.java
+++ src/test/java/framework/util/net/HttpBuilderTest.java
@@ -1,9 +1,62 @@
-package framework.util.net;
+package de.trinext.framework.util.net;
 
-/**
- * @author Dennis Woithe
- */
-final class HttpBuilderTest {
-    // TODO: Implement me!
-    // https://github.com/TriNext/framework-util/issues/13
+
+import de.trinext.framework.util.net.HttpBuilder.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpResponse;
+
+public class HttpBuilderTest {
+
+    @BeforeEach
+    public void setUp() {
+        // Wird noch vorher was gemacht? @Dennis
+    }
+
+    @Test
+    public void testForUrlWithStringShouldCreateWithUrlInstance() {
+        String url = "https://www.example.com/status";
+        HttpBuilderWithUrl result = HttpBuilder.forUrl(url);
+        assertNotNull(result, "The result should not be null.");
+        // Further assertions for HttpBuilderWithUrl if it has methods to retrieve its internal state
+    }
+
+    @Test
+    public void testForUrlWithURIShouldCreateWithUrlInstance() {
+        URI uri = URI.create("https://www.example.com/status");
+        HttpBuilderWithUrl result = HttpBuilder.forUrl(uri);
+        assertNotNull(result, "The result should not be null.");
+        // Further assertions for HttpBuilderWithUrl if it has methods to retrieve its internal state
+    }
+
+    @Test
+    public void testForUrlWithNullShouldThrowException() {
+        assertThrows(NullPointerException.class, () -> {
+            HttpBuilder.forUrl((URI) null);
+        }, "Passing a null URI should throw a NullPointerException.");
+    }
+
+    @Test
+    public void testHttpBuilderInstance() {
+        assertNotNull(HttpBuilder.DEFAULT_CLIENT, "Default client should be instantiated");
+        // Further checks can be performed based on how `HttpBuilder` is used
+    }
+
+    @Test
+    public void testHttpBuilderWithUrlConstructionWithValidUrl() {
+        String url = "https://www.example.com";
+        HttpBuilderWithUrl httpBuilderWithUrl = HttpBuilder.forUrl(url);
+        assertNotNull(httpBuilderWithUrl, "HttpBuilderWithUrl instance sollte nicht null null sein");
+        // More assertions to check the state of httpBuilderWithUrl if possible
+    }
+
+    @Test
+    public void testHttpBuilderWithUrlConstructionWithInvalidUrl() {
+        String url = "invalid-url";
+        assertThrows(IllegalArgumentException.class, () -> HttpBuilder.forUrl(url), "Invalide URL");
+    }
 }
