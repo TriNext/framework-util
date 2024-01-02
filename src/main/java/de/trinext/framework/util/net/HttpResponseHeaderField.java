ba967cb04971b96d3d825c3498a77ac4904c6184
TriNextSep <150134652+TriNextSep@users.noreply.github.com>
2024-01-02 16:11:10 +0100

    Refactored HttpBuilderTest and expanded HttpResponseHeaderField enum

--- src/main/java/de/trinext/framework/util/net/HttpResponseHeaderField.java
+++ src/main/java/de/trinext/framework/util/net/HttpResponseHeaderField.java
@@ -1,10 +1,89 @@
 package de.trinext.framework.util.net;
 
 /**
- * @author Dennis Woithe
+ * A list of common Http-Response-Header fields.
+ *
+ * @author Seppo
+ * @see <a href="https://en.wikipedia.org/wiki/List_of_HTTP_header_fields#Response_fields">Common Response Headers</a>
  */
+@SuppressWarnings({"DuplicateStringLiteralInspection", "unused", "SpellCheckingInspection"})
 public enum HttpResponseHeaderField {
 
-    // TODO: Implement https://en.wikipedia.org/wiki/List_of_HTTP_header_fields#Response_fields
+    ACCEPT_CH("Accept-CH"),
+    ACCESS_CONTROL_ALLOW_ORIGIN("Access-Control-Allow-Origin"),
+    ACCESS_CONTROL_ALLOW_CREDENTIALS("Access-Control-Allow-Credentials"),
+    ACCESS_CONTROL_EXPOSE_HEADERS("Access-Control-Expose-Headers"),
+    ACCESS_CONTROL_MAX_AGE("Access-Control-Max-Age"),
+    ACCESS_CONTROL_ALLOW_METHODS("Access-Control-Allow-Methods"),
+    ACCESS_CONTROL_ALLOW_HEADERS("Access-Control-Allow-Headers"),
+    ACCEPT_PATCH("Accept-Patch"),
+    ACCEPT_RANGES("Accept-Ranges"),
+    AGE("Age"),
+    ALLOW("Allow"),
+    ALT_SVC("Alt-Svc"),
+    CACHE_CONTROL("Cache-Control"),
+    CONNECTION("Connection"),
+    CONTENT_DISPOSITION("Content-Disposition"),
+    CONTENT_ENCODING("Content-Encoding"),
+    CONTENT_LANGUAGE("Content-Language"),
+    CONTENT_LENGTH("Content-Length"),
+    CONTENT_LOCATION("Content-Location"),
+    CONTENT_MD5("Content-MD5"),
+    CONTENT_RANGE("Content-Range"),
+    CONTENT_TYPE("Content-Type"),
+    DATE("Date"),
+    DELTA_BASE("Delta-Base"),
+    ETAG("ETag"),
+    EXPIRES("Expires"),
+    IM("IM"),
+    LAST_MODIFIED("Last-Modified"),
+    LINK("Link"),
+    LOCATION("Location"),
+    P3P("P3P"),
+    PRAGMA("Pragma"),
+    PREFERENCE_APPLIED("Preference-Applied"),
+    PROXY_AUTHENTICATE("Proxy-Authenticate"),
+    PUBLIC_KEY_PINS("Public-Key-Pins"),
+    RETRY_AFTER("Retry-After"),
+    SERVER("Server"),
+    SET_COOKIE("Set-Cookie"),
+    STRICT_TRANSPORT_SECURITY("Strict-Transport-Security"),
+    TRAILER("Trailer"),
+    TRANSFER_ENCODING("Transfer-Encoding"),
+    TK("Tk"),
+    UPGRADE("Upgrade"),
+    VARY("Vary"),
+    VIA("Via"),
+    WARNING("Warning"),
+    WWW_AUTHENTICATE("WWW-Authenticate"),
+    X_FRAME_OPTIONS("X-Frame-Options"),
+    CONTENT_SECURITY_POLICY("Content-Security-Policy"),
+    X_CONTENT_SECURITY_POLICY("X-Content-Security-Policy"),
+    X_WEBKIT_CSP("X-WebKit-CSP"),
+    EXPECT_CT("Expect-CT"),
+    NEL("NEL"),
+    PERMISSIONS_POLICY("Permissions-Policy"),
+    REFRESH("Refresh"),
+    REPORT_TO("Report-To"),
+    STATUS("Status"),
+    TIMING_ALLOW_ORIGIN("Timing-Allow-Origin"),
+    X_CONTENT_DURATION("X-Content-Duration"),
+    X_CONTENT_TYPE_OPTIONS("X-Content-Type-Options"),
+    X_POWERED_BY("X-Powered-By"),
+    X_REDIRECT_BY("X-Redirect-By"),
+    X_REQUEST_ID("X-Request-ID"),
+    X_CORRELATION_ID("X-Correlation-ID"),
+    X_UA_COMPATIBLE("X-UA-Compatible"),
+    X_XSS_PROTECTION("X-XSS-Protection");
+
+    private final String header;
+
+    HttpResponseHeaderField(String header) {
+        this.header = header;
+    }
+
+    public String getHeader() {
+        return header;
+    }
 
 }
--- src/test/java/framework/util/net/HttpBuilderTest.java
+++ src/test/java/framework/util/net/HttpBuilderTest.java
@@ -1,63 +1,55 @@
 package framework.util.net;
 
 
+import de.trinext.framework.util.net.HttpBuilder;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 import java.net.URI;
 
-import de.trinext.framework.util.net.HttpBuilder;
-import de.trinext.framework.util.net.HttpBuilder.HttpBuilderWithUrl;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+final class HttpBuilderTest {
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-
-public class HttpBuilderTest {
-
-    @BeforeEach
-    public void setUp() {
-        // Wird noch vorher was gemacht? @Dennis
-    }
+    private static final String VALID_URL = "https://www.example.com/status";
+    private static final String INVALID_URL = "|";
 
     @Test
-    public void testForUrlWithStringShouldCreateWithUrlInstance() {
-        String url = "https://www.example.com/status";
-        HttpBuilderWithUrl result = HttpBuilder.forUrl(url);
-        assertNotNull(result, "The result should not be null.");
+    void test_for_url_with_string_should_create_with_url_instance() {
+        var url = VALID_URL;
+        var result = HttpBuilder.forUrl(url);
+        assertNotNull(result);
         // Further assertions for HttpBuilderWithUrl if it has methods to retrieve its internal state
     }
 
     @Test
-    public void testForUrlWithURIShouldCreateWithUrlInstance() {
-        URI uri = URI.create("https://www.example.com/status");
-        HttpBuilderWithUrl result = HttpBuilder.forUrl(uri);
-        assertNotNull(result, "The result should not be null.");
+    void test_for_url_with_u_r_i_should_create_with_url_instance() {
+        var uri = URI.create(VALID_URL);
+        var result = HttpBuilder.forUrl(uri);
+        assertNotNull(result);
         // Further assertions for HttpBuilderWithUrl if it has methods to retrieve its internal state
     }
 
     @Test
-    public void testForUrlWithNullShouldThrowException() {
+    void test_for_url_with_null_should_throw_exception() {
         assertThrows(NullPointerException.class, () -> {
             HttpBuilder.forUrl((URI) null);
         }, "Passing a null URI should throw a NullPointerException.");
     }
 
     @Test
-    public void testHttpBuilderInstance() {
+    void test_http_builder_instance() {
         assertNotNull(HttpBuilder.DEFAULT_CLIENT, "Default client should be instantiated");
         // Further checks can be performed based on how `HttpBuilder` is used
     }
 
     @Test
-    public void testHttpBuilderWithUrlConstructionWithValidUrl() {
-        String url = "https://www.example.com";
-        HttpBuilderWithUrl httpBuilderWithUrl = HttpBuilder.forUrl(url);
+    void test_http_builder_with_url_construction_with_valid_url() {
+        var httpBuilderWithUrl = HttpBuilder.forUrl(VALID_URL);
         assertNotNull(httpBuilderWithUrl, "HttpBuilderWithUrl instance sollte nicht null null sein");
         // More assertions to check the state of httpBuilderWithUrl if possible
     }
 
     @Test
-    public void testHttpBuilderWithUrlConstructionWithInvalidUrl() {
-        String url = "invalid   url";
-        assertThrows(IllegalArgumentException.class, () -> HttpBuilder.forUrl(url), "Invalide URL");
+    void test_http_builder_with_url_construction_with_invalid_url() {
+        assertThrows(IllegalArgumentException.class, () -> HttpBuilder.forUrl(INVALID_URL), "Invalide URL");
     }
 }
